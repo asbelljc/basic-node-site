a8587d5e37bfde4f469f743e645af97245e55bc8
asbelljc <asbelljc@gmail.com>
2021-11-12 00:47:09 -0500

    Expressied app.

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-const http = require('http');
-const fs = require('fs');
-const port = process.env.port || 8080;
+const express = require('express');
+const app = express();
+const port = 8080;
 
-http
-  .createServer((req, res) => {
-    fs.readFile(
-      `./${req.url === '/' ? 'index' : req.url}.html`,
-      'utf-8',
-      (error, data) => {
-        if (error) {
-          fs.readFile('./404.html', (error, data) => {
-            res.writeHead(404, { 'Content-type': 'text/html' });
-            res.end(data, 'utf-8');
-          });
-        } else {
-          res.writeHead(200, { 'Content-type': 'text/html' });
-          res.end(data, 'utf-8');
-        }
-      }
-    );
-  })
-  .listen(port);
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile('./about.html', { root: __dirname });
+});
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile('./contact-me.html', { root: __dirname });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile('./404.html', { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
+
+// VANILLA NODE VERSION BELOW /////////////////////////////////////////////////
+
+// const http = require('http');
+// const fs = require('fs');
+// const port = process.env.port || 8080;
+
+// http
+//   .createServer((req, res) => {
+//     fs.readFile(
+//       `./${req.url === '/' ? 'index' : req.url}.html`,
+//       'utf-8',
+//       (error, data) => {
+//         if (error) {
+//           fs.readFile('./404.html', (error, data) => {
+//             res.writeHead(404, { 'Content-type': 'text/html' });
+//             res.end(data, 'utf-8');
+//           });
+//         } else {
+//           res.writeHead(200, { 'Content-type': 'text/html' });
+//           res.end(data, 'utf-8');
+//         }
+//       }
+//     );
+//   })
+//   .listen(port);
